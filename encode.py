cba317e90f1b2c4b8bd2d8ffc02f964ad576cc8d
manuamest <manuamest@gmail.com>
2023-03-22 18:22:02 +0100

    Encode, shingokiKB y ejemplos

---
+++ encode.py
@@ -0,0 +1,31 @@
+import sys
+
+def read_matrix(file):
+    matrix = []
+    with open(file, 'r') as f:
+        for line in f:
+            row = list(map(int, line.strip().split()))
+            matrix.append(row)
+    return matrix
+
+def generate_lp(matrix, output_file):
+    size = len(matrix)
+    with open(output_file, 'w') as f:
+        f.write(f'size({size}).\n')
+        for i in range(size):
+            for j in range(size):
+                if matrix[i][j] != 0:
+                    f.write(f'number(({i},{j}), {matrix[i][j]}).\n')
+
+def main(input_file, output_file):
+    matrix = read_matrix(input_file)
+    generate_lp(matrix, output_file)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 encode.py input_file output_file")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    main(input_file, output_file)
